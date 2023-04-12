f3145351f7e28cdc513afcdad36df056887ff445
Onésimo Martins <Ogouveia86@gmail.com>
2023-04-12 23:13:24 +0100

    chore: first commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/marti/CLionProjects/CPD
+# Install script for directory: C:/Users/marti/Java/Projectos/Isptec/CPD
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/marti/CLionProjects/CPD/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/marti/Java/Projectos/Isptec/CPD/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
